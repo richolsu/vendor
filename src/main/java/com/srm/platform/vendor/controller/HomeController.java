0a1ff2557fc717261a19a7948e4c122fdf9d9a65
richolsu <38938914+richolsu@users.noreply.github.com>
2018-06-04 22:50:48 +0700

    add security

--- src/main/java/com/srm/platform/vendor/controller/HomeController.java
+++ src/main/java/com/srm/platform/vendor/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.srm.platform.vendor.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +12,27 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home() {
 		return "home/index";
+	}
+
+	@RequestMapping("/admin")
+	public String admin() {
+
+		return "home/index";
+	}
+
+	@RequestMapping(value = "/signin")
+	public String login(HttpServletRequest request, Model model) {
+		return "signin";
+	}
+
+	@RequestMapping(value = "/signout")
+	public String logout() {
+		return "signout";
+	}
+
+	@RequestMapping(value = "/denied")
+	public String denied() {
+		return "denied";
 	}
 
 }
---
+++ src/main/java/com/srm/platform/vendor/security/AppUserDetailsServiceDAO.java
@@ -0,0 +1,65 @@
+package com.srm.platform.vendor.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserDetailsServiceDAO implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+
+		if (!username.equals("user")) {
+			throw new UsernameNotFoundException(username + " not found");
+		}
+
+		// creating dummy user details, should do JDBC operations
+		return new UserDetails() {
+
+			private static final long serialVersionUID = 2059202961588104658L;
+
+			@Override
+			public boolean isEnabled() {
+				return true;
+			}
+
+			@Override
+			public boolean isCredentialsNonExpired() {
+				return true;
+			}
+
+			@Override
+			public boolean isAccountNonLocked() {
+				return true;
+			}
+
+			@Override
+			public boolean isAccountNonExpired() {
+				return true;
+			}
+
+			@Override
+			public String getUsername() {
+				return username;
+			}
+
+			@Override
+			public String getPassword() {
+				return "111";
+			}
+
+			@Override
+			public Collection<? extends GrantedAuthority> getAuthorities() {
+				List<SimpleGrantedAuthority> auths = new java.util.ArrayList<>();
+				auths.add(new SimpleGrantedAuthority("ROLE_USER"));
+				return auths;
+			}
+		};
+	}
+
+}
